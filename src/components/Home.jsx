ef4af4d80c71c7a3814a1b17ca9eb095e6c0daba
savan Kumar <121648350+ShivSaini011@users.noreply.github.com>
2024-12-26 21:51:01 +0530

    card raddy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,8 @@
 // import Header from "./Home/Header";
 import AppDownload from "./Home/AppDownload";
 import BannerSlider from "./Home/BannerSlider";
-<<<<<<< HEAD
+
 // import ProductList from "./Home/ProductList";
-=======
-import ProductList from "./Home/ProductList";
-<<<<<<< Updated upstream
-=======
->>>>>>> 42c11c4a561a4a38e30d020f8295492514e668f3
->>>>>>> Stashed changes
 import ProductSlider from "./Home/ProductSlider";
 import ResponsiveHeader from "./Home/ResponsiveHeader";
 import Services from "./Home/Services";
